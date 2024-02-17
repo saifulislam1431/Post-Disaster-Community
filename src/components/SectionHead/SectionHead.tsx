f9d168a2d5e0faf280c686f4af1589cf4e52ad21
Saiful Islam <sakibbhaiya143@gmail.com>
2024-02-17 18:19:06 +0600

    Our partner section added

--- src/components/SectionHead/SectionHead.tsx
+++ src/components/SectionHead/SectionHead.tsx
@@ -1,4 +1,4 @@
-const SectionHead = ({ title, description }: { title: string, description: string }) => {
+const SectionHead = ({ title, description }: { title: string, description?: string }) => {
     return (
         <div className="text-center">
             <h1 className="brandFont text-3xl mb-3">{title}</h1>
--- src/pages/Home/LandingComponents/OurPartner/OurPartner.tsx
+++ src/pages/Home/LandingComponents/OurPartner/OurPartner.tsx
@@ -1,8 +1,38 @@
+import SectionHead from "@/components/SectionHead/SectionHead";
+import Marquee from "react-fast-marquee";
+import brand1 from "../../../../assets/brand/brand1.png"
+import brand2 from "../../../../assets/brand/brand2.png"
+import brand3 from "../../../../assets/brand/brand3.png"
+import brand4 from "../../../../assets/brand/brand4.png"
+import brand5 from "../../../../assets/brand/brand5.png"
+
 const OurPartner = () => {
     return (
-        <div>
-            OurPartner
-        </div>
+        <section>
+            <SectionHead title="Top Contributor" />
+
+            <div className="my-20">
+                <Marquee speed={30} gradient={true} gradientColor="255, 255, 255">
+                    <div className="flex items-center space-x-28 justify-center">
+                        <div>
+                            <img src={brand1} alt="brand1" className="w-32" />
+                        </div>
+                        <div>
+                            <img src={brand2} alt="brand2" className="w-32" />
+                        </div>
+                        <div>
+                            <img src={brand3} alt="brand3" className="w-32" />
+                        </div>
+                        <div>
+                            <img src={brand4} alt="brand4" className="w-32" />
+                        </div>
+                        <div>
+                            <img src={brand5} alt="brand5" className="w-32" />
+                        </div>
+                    </div>
+                </Marquee>
+            </div>
+        </section>
     );
 };
 
