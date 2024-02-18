6ba0c16cc2faaba988c6b967948c982fc80fa22a
Saiful Islam <sakibbhaiya143@gmail.com>
2024-02-18 20:10:27 +0600

    Auth APi added

---
+++ src/redux/features/auth/authAPi.ts
@@ -0,0 +1,23 @@
+import { baseApi } from "@/redux/baseApi/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+    endpoints:(builder)=>({
+        register: builder.mutation({
+            query:(userInfo)=>({
+                url:"register",
+                method:"POST",
+                body: userInfo
+            })
+        }),
+        login: builder.mutation({
+            query:(userInfo)=>({
+                url:"login",
+                method:"POST",
+                body: userInfo
+            })
+        })
+    })
+})
+
+
+export const {useRegisterMutation, useLoginMutation} = authApi;
